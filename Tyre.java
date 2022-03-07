00cf598f3793d39ec24cb6ed308d40913559c342
YashwanthMahi1998 <100289957+YashwanthMahi1998@users.noreply.github.com>
2022-03-07 19:46:22 +0530

    Add files via upload

---
+++ Tyre.java
@@ -0,0 +1,33 @@
+package com.training.Tcs;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Tyre {
+	
+
+	public Tyre() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	private String tyrebrand;
+
+	public Tyre(String tyrebrand) {
+		super();
+		this.tyrebrand = tyrebrand;
+	}
+
+	public String getTyrebrand() {
+		return tyrebrand;
+	}
+
+	public void setTyrebrand(String tyrebrand) {
+		this.tyrebrand = tyrebrand;
+	}
+
+	@Override
+	public String toString() {
+		return "Tyre [tyrebrand=" + tyrebrand + "]";
+	}
+	
+	}
